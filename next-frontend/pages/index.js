38cb6ee468150da6c5014a3362eedca9b4fd93b7
arfat-xyz <arfatrahman08@gmail.com>
2023-01-30 13:27:18 +0600

    rename url

--- next-frontend/pages/index.js
+++ next-frontend/pages/index.js
@@ -1,14 +1,24 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
+import { createClient } from "next-sanity";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
+export default function Home({ blogs }) {
+  console.log(blogs);
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
+}
+export async function getServerSideProps() {
+  const client = createClient({
+    projectId: "w3pjopko",
+    dataset: "production",
+    apiVersion: "2022-03-25",
+    useCdn: false,
+  });
+  const blogs = await client.fetch(`*[_type == "blog"]`);
+  return {
+    props: {
+      blogs,
+    },
+  };
 }
