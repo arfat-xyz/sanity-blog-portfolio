e0922e7bcaeebf96109dd135477c2ddbc839b1fb
arfat-xyz <arfatrahman08@gmail.com>
2023-02-05 15:31:51 +0600

    some bug and added blog page

--- next-frontend/pages/blog/[slug].js
+++ next-frontend/pages/blog/[slug].js
@@ -13,7 +13,7 @@ const Post = ({ blog, author }) => {
     useCdn: false,
   });
   const builder = imageUrlBuilder(client);
-  console.log("blog", blog);
+  // console.log("blog", blog);
   // console.log("author", author);
   return (
     <>
@@ -230,7 +230,6 @@ const Post = ({ blog, author }) => {
                   // Optionally override marks, decorators, blocks, etc. in a flat
                   // structure without doing any gymnastics
                   serializers={{
-                    h1: (props) => <h1 style={{ color: "red" }} {...props} />,
                     image: (props) => (
                       <>
                         <div className="text-center">
@@ -248,126 +247,29 @@ const Post = ({ blog, author }) => {
                     ),
                   }}
                 />
-
-                <h2 id="lorem-ipsum-dolor-sit-amet">
-                  Lorem ipsum dolor sit amet
-                </h2>
-
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </p>
-
-                <p>
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                  accusantium doloremque laudantium, totam rem aperiam, eaque
-                  ipsa quae ab illo inventore veritatis et quasi architecto
-                  beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
-                  quia voluptas sit aspernatur aut odit aut fugit, sed quia
-                  consequuntur magni dolores eos qui ratione voluptatem sequi
-                  nesciunt. Neque porro quisquam est. Ut enim ad minima veniam,
-                  quis nostrum exercitationem ullam corporis suscipit
-                  laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis
-                  autem vel eum iure reprehenderit qui in ea voluptate velit
-                  esse quam nihil molestiae consequatur, vel illum qui dolorem
-                  eum fugiat quo voluptas nulla pariatur?
-                </p>
-
-                <h3 id="lorem-ipsum-dolor-sit-amet-1">
-                  Lorem ipsum dolor sit amet
-                </h3>
-
-                <p>
-                  Possent quippe. Conata fera ab Hersilie iam auditur haesit.
-                  Culpa ignis retia Aonides ictus crescendo micantes, petere,
-                  ambustaque vicem in digitos iubet cornigeris.
-                </p>
-
-                <ul>
-                  <li>Iura demens satus vino simulac celanda</li>
-                  <li>Exhibuere frustra utque</li>
-                  <li>Iura demens satus vino simulac celanda</li>
-                  <li>Exhibuere frustra utque</li>
-                  <li>Panda pars umidus</li>
-                </ul>
-
-                <h4 id="lorem-ipsum-dolor-sit-amet-2">
-                  Lorem ipsum dolor sit amet
-                </h4>
-
-                <p>
-                  Possent quippe. Conata fera ab Hersilie iam auditur haesit.
-                  Culpa ignis retia Aonides ictus crescendo micantes, petere,
-                  ambustaque vicem in digitos iubet cornigeris.
-                </p>
-
-                <div class="language-plaintext highlighter-rouge">
-                  <div class="highlight"></div>
-                </div>
-
-                <p>
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                  accusantium doloremque laudantium, totam rem aperiam, eaque
-                  ipsa quae ab illo inventore veritatis et quasi architecto
-                  beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
-                  quia voluptas sit aspernatur aut odit aut fugit, sed quia
-                  consequuntur magni dolores eos qui ratione voluptatem sequi
-                  nesciunt. Neque porro quisquam est, qui dolorem ipsum quia
-                  dolor sit amet, consectetur, adipisci velit.
-                </p>
-              </div>
-              <div class="flex pt-10">
-                <a
-                  href="/"
-                  class="rounded-xl bg-primary px-4 py-1 font-body font-bold text-white hover:bg-grey-20"
-                >
-                  Frontend
-                </a>
-                <a
-                  href="/"
-                  class="ml-2 block rounded-xl bg-primary px-4 py-1 font-body font-bold text-white hover:bg-grey-20"
-                >
-                  Design
-                </a>
-              </div>
-              <div class="mt-10 flex justify-between border-t border-lila py-12">
-                <a href="/" class="flex items-center">
-                  <i class="bx bx-left-arrow-alt text-2xl text-primary"></i>
-                  <span class="block pl-2 font-body text-lg font-bold uppercase text-primary md:pl-5">
-                    Previous Post
-                  </span>
-                </a>
-                <a href="/" class="flex items-center">
-                  <span class="block pr-2 font-body text-lg font-bold uppercase text-primary md:pr-5">
-                    Next Post
-                  </span>
-                  <i class="bx bx-right-arrow-alt text-2xl text-primary"></i>
-                </a>
               </div>
               <div class="flex flex-col items-center border-t border-lila py-12 pt-12 md:flex-row md:items-start xl:pb-20">
                 <div class="w-3/4 sm:w-2/5 lg:w-1/4 xl:w-1/5">
                   <img
-                    src="/assets/img/blog-author.jpg"
+                    src={builder
+                      .image(author.authorimage)
+                      .width(200)
+                      .height(200)
+                      .fit("fillmax")
+                      .url()}
                     class="rounded-full shadow"
                     alt="author image"
                   />
                 </div>
                 <div class="ml-0 text-center md:ml-10 md:w-5/6 md:text-left">
+                  <span className="bg-gray-400 px-5 py-1 rounded-md mb-3">
+                    Author
+                  </span>
                   <h3 class="pt-10 font-body text-2xl font-bold text-secondary md:pt-0">
-                    Christy Smith
+                    {author.name}
                   </h3>
                   <p class="pt-5 font-body text-lg leading-8 text-secondary sm:leading-9 md:text-xl md:leading-9 lg:leading-9 xl:leading-9">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit.
+                    {author.desc}
                   </p>
                   <div class="flex items-center justify-center pt-5 md:justify-start">
                     <a href="/">
@@ -395,7 +297,7 @@ const Post = ({ blog, author }) => {
         <div class="bg-primary">
           <div class="container flex flex-col justify-between py-6 sm:flex-row">
             <p class="text-center font-body text-white md:text-left">
-              © Copyright 2022. All right reserved, ATOM.
+              © Copyright 2022. All right reserved, Arfat.xyz.
             </p>
             <div class="flex items-center justify-center pt-5 sm:justify-start sm:pt-0">
               <a href="/">
--- next-frontend/pages/index.js
+++ next-frontend/pages/index.js
@@ -241,7 +241,7 @@ export default function Home({ blogs, author }) {
                   </div>
                   <div className="pt-8 sm:pt-10 lg:pl-8 lg:pt-0">
                     <h1 className="text-center font-header text-4xl text-white sm:text-left sm:text-5xl md:text-6xl">
-                      Hello I'm Christy Smith!
+                      Hello I'm {author.name}!
                     </h1>
                     <div className="flex flex-col justify-center pt-3 sm:flex-row sm:pt-5 lg:justify-start">
                       <div className="flex items-center justify-center pl-0 sm:justify-start md:pl-1">
