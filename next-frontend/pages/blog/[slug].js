cf7fd5062b7a20d08fb94c5a27529a0769b302df
arfat-xyz <arfatrahman08@gmail.com>
2023-02-05 16:57:07 +0600

    Some bug fixed--3

--- next-frontend/pages/blog/[slug].js
+++ next-frontend/pages/blog/[slug].js
@@ -101,11 +101,7 @@ const Post = ({ blog, author }) => {
         {/* <div className="pointer-events-none fixed inset-0 z-70 min-h-screen bg-black bg-opacity-70 opacity-0 transition-opacity lg:hidden">
           <div className="absolute right-0 min-h-screen w-2/3 bg-primary py-4 px-8 shadow md:w-1/3">
             <button className="absolute top-0 right-0 mt-4 mr-4">
-              <img
-                src="/assets/img/icon-close.svg"
-                className="h-10 w-auto"
-                alt=""
-              />
+              
             </button>
 
             <ul className="mt-8 flex flex-col">
